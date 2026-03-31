745f6cea655eb57ac8fcc4d2feb41d259c27279b
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-03-31 12:19:29 +0530

    Add Manufacturers component to display manufacturer data

---
+++ app/manufacturers/page.js
@@ -0,0 +1,29 @@
+import manufacturers from "../../data/manufacturers"
+
+export default function Manufacturers(){
+
+return(
+
+<div style={{padding:40}}>
+
+<h1>Manufacturers</h1>
+
+{manufacturers.map((m)=>(
+
+<div key={m.id} style={{marginBottom:20}}>
+
+<h3>{m.name}</h3>
+
+<p>Category: {m.category}</p>
+
+<p>City: {m.city}</p>
+
+</div>
+
+))}
+
+</div>
+
+)
+
+}
