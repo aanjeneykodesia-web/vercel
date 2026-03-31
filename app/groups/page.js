e0ed332a731783abc65ea32841117fe2b33f0399
aanjeneykodesia-web <aanjeneykodesia@gmail.com>
2026-03-31 12:20:00 +0530

    Add Groups component to display manufacturer groups

---
+++ app/groups/page.js
@@ -0,0 +1,34 @@
+import manufacturers from "../../data/manufacturers"
+import {groupManufacturers} from "../../lib/group"
+
+export default function Groups(){
+
+const groups = groupManufacturers(manufacturers)
+
+return(
+
+<div style={{padding:40}}>
+
+<h1>Manufacturer Groups</h1>
+
+{Object.keys(groups).map((category)=>(
+
+<div key={category} style={{marginBottom:30}}>
+
+<h2>{category}</h2>
+
+{groups[category].map((m)=>(
+
+<p key={m.id}>{m.name} - {m.city}</p>
+
+))}
+
+</div>
+
+))}
+
+</div>
+
+)
+
+}
